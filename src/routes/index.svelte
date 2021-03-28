cecfea5031af49197ece38ca21ac8630d0e0b361
prinpadure <rares.holota@gmail.com>
2021-03-28 22:08:38 +0300

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1>My Todos</h1>
+
+</main>
+
+<style lang="scss">
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	main {
+		@apply text-center;
+		@apply p-4;
+		@apply mx-auto;
+	}
+
+	h1 {
+		@apply text-red-600;
+		@apply uppercase;
+		@apply text-6xl;
+		@apply font-thin;
+		@apply leading-tight;
+		@apply my-16 mx-auto;
+		@apply max-w-xs;
+	}
+
+	p {
+		@apply max-w-xs;
+		@apply my-8 mx-auto;
+		@apply leading-snug;
+	}
+
+	@screen sm {
+		h1 {
+			@apply max-w-none;
+		}
+
+		p {
+			@apply max-w-none;
+		}
+	}
+</style>
---
+++ src/routes/todos/index.ts
@@ -0,0 +1,9 @@
+const get = async (request) => {
+	return {
+		status: 200,
+		body: {
+			todo: "todo"
+		}
+	};
+};
+export { get };
