bfe4cb3a4f3b4ed29cc8a180e1924b20c973be3d
prinpadure <rares.holota@gmail.com>
2021-03-29 21:56:11 +0300

    use mongoose

--- src/routes/todos/index.ts
+++ src/routes/todos/index.ts
@@ -1,4 +1,25 @@
-const get = async (request) => {
+const get = async (request): Promise<any> => {
+	const completed = request.query.get("completed") === "true";
+	return {
+		status: 200,
+		body: {
+			completed
+		}
+	};
+};
+
+const post = async (request) => {
+	const completed = request.query;
+	return {
+		status: 200,
+		body: {
+			completed
+		}
+	};
+};
+
+const put = async (request) => {
+	const completed = request.query;
 	return {
 		status: 200,
 		body: {
@@ -6,4 +27,15 @@ const get = async (request) => {
 		}
 	};
 };
+
+const del = async (request) => {
+	const completed = request.query;
+	return {
+		status: 200,
+		body: {
+			todo: "todo"
+		}
+	};
+};
+
 export { get };
